40ed16070e4e4a78a864d1277bdeacbdafc6de47
Grant Miller <grant.miller@sas.com>
2025-04-29 16:25:40 -0400

    lock/unlock NS is working

---
+++ gm_nintendo_parental/__init__.py
@@ -0,0 +1,283 @@
+import time
+from enum import Enum
+from urllib.parse import urlencode
+
+import requests
+
+from gm_nintendo_parental.const import CLIENT_ID, REDIRECT_URI, SCOPE, AUTHORIZE_URL, SESSION_TOKEN_URL, TOKEN_URL, \
+    GRANT_TYPE, MY_ACCOUNT_ENDPOINT, USER_AGENT, MOBILE_APP_PKG, OS_NAME, OS_VERSION, DEVICE_MODEL, MOBILE_APP_VERSION, \
+    MOBILE_APP_BUILD, ENDPOINTS, BASE_URL
+from gm_nintendo_parental.helpers import random_string, hash_it, parse_response_url
+
+
+class ParentalControl:
+    def __init__(self, save_state_callback=None, load_state_callback=None, callback_kwargs={}):
+        '''
+        If you need to re-hydrate this object, you can use
+        the save/load callbacks to restore the state.
+
+        :param save_state_callback:
+        :param load_state_callback:
+        '''
+        self.save_state_callback = save_state_callback
+        self.load_state_callback = load_state_callback
+        self.callback_kwargs = callback_kwargs
+
+        self.verification_code = None
+        self.session_token = None
+        self.access_token = None
+        self.access_token_expires_timestamp = 0
+        self.id_token = None
+        self.account_id = None
+
+        self._load()
+
+    def get_auth_url(self):
+        '''
+        The user should go to this link.
+        Assuming you are already logged in, you will
+        see a "select this account" button.
+        Right-click on that button and copy the link.
+        You will paste that link into process_auth_link() method.
+        :return:
+        '''
+        # Generate a temporary code that will be used to verify
+        # the server response.
+        # That way we know the response actually came from the server.
+        self.verification_code = random_string()
+        if self.save_state_callback:
+            self._save(verification_code=self.verification_code)
+
+        # build the login url
+        params = {
+            "client_id": CLIENT_ID,
+            # "interacted": 1,
+            "redirect_uri": REDIRECT_URI,
+            "response_type": "session_token_code",
+            "scope": SCOPE,
+            "session_token_code_challenge": hash_it(self.verification_code),
+            "session_token_code_challenge_method": "S256",
+            "state": self.verification_code,
+            "theme": "login_form"
+        }
+        login_url = AUTHORIZE_URL.format(urlencode(params)).replace("%2B", "+")
+        return login_url
+
+    def process_auth_link(self, link: str):
+        '''
+
+        :param link: string like 'npf54789befb391a838://auth#session_token_code=really-long-string&state=verification-code-here&session_state=abc123-'
+        :return:
+        '''
+        # pull out the important info from the link
+        data = parse_response_url(link)
+        data['client_id'] = CLIENT_ID
+        data['session_token_code_verifier'] = self.verification_code
+
+        # trade our session_token_code for a session_token
+        resp = requests.post(
+            url=SESSION_TOKEN_URL,
+            data=data,
+            headers={
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Accept': 'application/json',
+                'User-Agent': 'NASDKAPI; Android',
+            }
+        )
+        self.session_token = resp.json().get('session_token')
+        self._save(session_token=self.session_token)
+
+        self.get_new_access_token()
+
+    def get_new_access_token(self):
+        # trade the session_token for an access_token
+
+        resp = requests.post(
+            url=TOKEN_URL,
+            json={
+                "client_id": CLIENT_ID,
+                "grant_type": GRANT_TYPE,
+                "session_token": self.session_token
+            }
+        )
+        if not resp.ok:
+            print(resp.text)
+
+        self.access_token = resp.json()['access_token']
+        self.id_token = resp.json()['id_token']
+        self.access_token_expires_timestamp = (
+                time.time() + resp.json()['expires_in']
+        )
+
+        self._save(
+            access_token=self.access_token,
+            id_token=self.id_token,
+            access_token_expires_timestamp=self.access_token_expires_timestamp
+        )
+
+        return self.access_token
+
+    def _save(self, **kwargs):
+
+        if self.save_state_callback:
+            d = self._load().copy()
+            d.update(**kwargs)
+            self.save_state_callback(**d)
+
+    def _load(self):
+        if self.load_state_callback:
+            data = self.load_state_callback(**self.callback_kwargs)
+
+            self.verification_code = data.get('verification_code', None) or random_string()
+            self.session_token = data.get('session_token', None)
+
+            self.access_token = data.get('access_token', None)
+            self.id_token = data.get('id_token', None)
+            self.access_token_expires_timestamp = data.get('access_token_expires_timestamp', 0)
+
+            self.account_id = data.get('account_id', None)
+            return data
+        else:
+            self.verification_code = random_string()
+
+        return {'verification_code': self.verification_code}
+
+    def get_access_token(self):
+        if self.access_token and self.access_token_expires_timestamp < time.time():
+            # the access token exist and is not expired
+            return self.access_token
+        else:
+            # the access token is missing or expired
+            time.sleep(1)
+            return self.get_new_access_token()
+
+    def get_account_id(self):
+
+        if self.account_id is None:
+            resp = requests.get(
+                url=MY_ACCOUNT_ENDPOINT,
+                headers={
+                    "Authorization": f"Bearer {self.get_access_token()}"
+                }
+            )
+            print('resp=', resp)
+            print(resp.json())
+            self.account_id = resp.json()['id']
+            self._save(account_id=self.account_id)
+
+        return self.account_id
+
+    def send_request(self, method='GET', *a, **k):
+        return requests.request(
+            method=method,
+            headers={
+                "Authorization": 'Bearer ' + self.get_access_token(),
+                "User-Agent": USER_AGENT,
+                "X-Moon-App-Id": MOBILE_APP_PKG,
+                "X-Moon-Os": OS_NAME,
+                "X-Moon-Os-Version": OS_VERSION,
+                "X-Moon-Model": DEVICE_MODEL,
+                "X-Moon-TimeZone": str(time.timezone),
+                "X-Moon-Os-Language": 'en-US',
+                "X-Moon-App-Language": 'en-US',
+                "X-Moon-App-Display-Version": MOBILE_APP_VERSION,
+                "X-Moon-App-Internal-Version": MOBILE_APP_BUILD,
+            },
+            *a, **k
+        )
+
+    def get_device(self, device_label: str):
+        # get the list of devices
+
+        resp = self.send_request(
+            'GET',
+            url=ENDPOINTS['get_account_devices']['url'].format(
+                BASE_URL=BASE_URL,
+                ACCOUNT_ID=self.get_account_id()
+            )
+
+        )
+        if not resp.ok:
+            print('error getting device', resp.text)
+
+        for dev in resp.json().get('items', []):
+            if dev.get('label', '') == device_label:
+                print('found device=', dev)
+                return dev
+        else:
+            print('device not found')
+            for dev in resp.json().get('items', []):
+                print('dev=', dev)
+        return None
+
+    def get_parental_control_settings(self, device):
+        data = self._load()
+        if device['deviceId'] not in data:
+
+            # resp = requests.get(
+            #     headers={
+            #         "Authorization": 'Bearer ' + self.get_access_token(),
+            #     },
+            #     url=BASE_URL + '/devices/' + device.get('deviceId') + '/parental_control_setting',
+            # )
+            resp = self.send_request(
+                'GET',
+                url=BASE_URL + '/devices/' + device.get('deviceId') + '/parental_control_setting',
+
+            )
+            if resp.ok:
+                print('get_parental_control_settings success')
+                print(resp.json())
+
+                self._save(**{
+                    device['deviceId']: resp.json()
+                })
+                data.update({
+                    device['deviceId']: resp.json()
+                })
+            else:
+                print('get settings failed', resp.text)
+        print('old settings=', data[device['deviceId']])
+        return data[device['deviceId']]
+
+    def lock_device(self, device_label: str, lock: bool):
+        state_name = 'FORCED_TERMINATION' if lock else 'ALARM'
+        device = self.get_device(device_label)
+        settings = self.get_parental_control_settings(device)
+
+        settings['playTimerRegulations']['restrictionMode'] = state_name
+        print('new settings=', settings)
+
+        resp = self.send_request(
+            "POST",
+            # url=BASE_URL + '/devices/' + device.get('deviceId') + '/parental_control_setting',
+            url=BASE_URL + '/devices/' + device.get('deviceId') + '/alarm_setting_state',
+
+            # json=json.dumps({
+            #     'status': 'VISIBLE' if lock else 'INVISIBLE'
+            # })
+            # .replace('"VISIBLE"', 'VISIBLE')
+            # .replace('"INVISIBLE"', 'INVISIBLE')
+
+            json={'status': 'TO_VISIBLE' if lock else 'TO_INVISIBLE'}
+        )
+        if resp.ok:
+            self._save(**{device['deviceId']: settings})
+        else:
+            print(resp.reason)
+            print(resp.headers)
+            print(resp.content)
+
+        print('lock_device', lock, 'ok=', resp.ok)
+
+
+class AlarmSettingState(Enum):
+    """Alarm setting states."""
+    SUCCESS = 0
+    TO_VISIBLE = 1
+    TO_INVISIBLE = 2
+    VISIBLE = 4
+    INVISIBLE = 8
+
+    def __str__(self) -> str:
+        return self.name
---
+++ test.py
@@ -0,0 +1,37 @@
+import os
+import json
+
+from gm_nintendo_parental import ParentalControl
+
+
+def save(**k):
+    with open('test.json', 'wt') as file:
+        file.write(json.dumps(k, indent=2))
+
+
+def load(**k):
+    if not os.path.isfile('test.json'):
+        return k
+
+    try:
+        with open('test.json', 'rt') as file:
+            return json.load(file)
+    except Exception as e:
+        print(e)
+        return k
+
+
+pc = ParentalControl(
+    save_state_callback=save,
+    load_state_callback=load,
+    callback_kwargs={'random': 'kwargs'}
+)
+
+if not pc.access_token:
+    if not pc.session_token:
+        print(pc.get_auth_url())
+        pc.process_auth_link(input('copy paste the link here:\n'))
+    else:
+        pc.get_new_access_token()
+
+pc.lock_device('Grant', True)
